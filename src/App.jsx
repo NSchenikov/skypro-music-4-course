e7ea1bccc6fcedc060558d32e68ff7307dade65c
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-08 02:41:15 +0300

    hw1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="line">Здесь скоро будет Skypro.Music</div>
+    </div>
+  )
+}
+
+export default App
