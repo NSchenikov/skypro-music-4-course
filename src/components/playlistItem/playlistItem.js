b32e85544d52d00c30722ffd27ad1a5f5ad08228
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-14 22:35:07 +0300

    props in PlayListItem destructured

--- src/components/playlistItem/playlistItem.js
+++ src/components/playlistItem/playlistItem.js
@@ -1,6 +1,12 @@
 import './playlistItem.css'
 
-function PlaylistItem(props) {
+function PlaylistItem({
+  trackTitle,
+  trackTitleExtra,
+  trackAuthor,
+  trackAlbum,
+  trackTime,
+}) {
   return (
     <div className="playlist__item">
       <div className="playlist__track track">
@@ -12,26 +18,26 @@ function PlaylistItem(props) {
           </div>
           <div className="track__title-text">
             <a className="track__title-link" href="http://">
-              {props.trackTitle}
-              <span className="track__title-span">{props.trackTitleExtra}</span>
+              {trackTitle}
+              <span className="track__title-span">{trackTitleExtra}</span>
             </a>
           </div>
         </div>
         <div className="track__author">
           <a className="track__author-link" href="http://">
-            {props.trackAuthor}
+            {trackAuthor}
           </a>
         </div>
         <div className="track__album">
           <a className="track__album-link" href="http://">
-            {props.trackAlbum}
+            {trackAlbum}
           </a>
         </div>
         <div className="track__time">
           <svg className="track__time-svg" alt="time">
             <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
           </svg>
-          <span className="track__time-text">{props.trackTime}</span>
+          <span className="track__time-text">{trackTime}</span>
         </div>
       </div>
     </div>
