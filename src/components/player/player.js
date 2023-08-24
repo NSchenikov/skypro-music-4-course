992eeb002df688a4b48dc503082b796d446bb1d8
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-24 23:22:19 +0300

    player partically styled by styled-components

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,39 +1,163 @@
 import './player.css'
+import styled from 'styled-components'
+
+const StyledBar = styled.div`
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgba(28, 28, 28, 0.5);
+`
+
+const StyledBarContent = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+`
+
+const StyledBarPlayerProgress = styled.div`
+  width: 100%;
+  height: 5px;
+  background: #2e2e2e;
+`
+
+const StyledBarPlayerBlock = styled.div`
+  height: 73px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+`
+
+const StyledBarPlayer = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+`
+
+const StyledPlayerControls = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  padding: 0 27px 0 31px;
+`
+
+const StyledPlayerBtnPrev = styled.div`
+  padding: 5px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-right: 23px;
+`
+
+const StyledPlayerBtnPlay = styled(StyledPlayerBtnPrev)`
+  margin-right: 23px;
+`
+
+const StyledPlayerBtnNext = styled(StyledPlayerBtnPrev)`
+  margin-right: 28px;
+  fill: #a53939;
+`
+
+const StyledPlayerBtnRepeat = styled(StyledPlayerBtnPrev)`
+  margin-right: 24px;
+`
+
+const StyledPlayerBtnShuffle = styled(StyledPlayerBtnPrev)`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`
+
+const StyledPlayerBtnPrevSvg = styled.svg`
+  width: 15px;
+  height: 14px;
+`
+
+const StyledPlayerBtnPlaySvg = styled.svg`
+  width: 22px;
+  height: 20px;
+  fill: #d9d9d9;
+`
+
+const StyledPlayerBtnRepeatSvg = styled.svg`
+  width: 18px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`
+
+const StyledPlayerBtnShuffleSvg = styled.svg`
+  width: 19px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`
+// const Styled = styled.div``
 
 function Player() {
   return (
-    <div className="bar">
-      <div className="bar__content">
-        <div className="bar__player-progress"></div>
-        <div className="bar__player-block">
-          <div className="bar__player player">
-            <div className="player__controls">
-              <div className="player__btn-prev">
-                <svg className="player__btn-prev-svg" alt="prev">
+    <StyledBar>
+      <StyledBarContent>
+        <StyledBarPlayerProgress />
+        <StyledBarPlayerBlock>
+          <StyledBarPlayer>
+            <StyledPlayerControls>
+              <StyledPlayerBtnPrev>
+                <StyledPlayerBtnPrevSvg alt="prev">
                   <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-                </svg>
-              </div>
-              <div className="player__btn-play _btn">
-                <svg className="player__btn-play-svg" alt="play">
+                </StyledPlayerBtnPrevSvg>
+              </StyledPlayerBtnPrev>
+              <StyledPlayerBtnPlay className="_btn">
+                <StyledPlayerBtnPlaySvg alt="play">
                   <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                </svg>
-              </div>
-              <div className="player__btn-next">
+                </StyledPlayerBtnPlaySvg>
+              </StyledPlayerBtnPlay>
+              <StyledPlayerBtnNext>
                 <svg className="player__btn-next-svg" alt="next">
                   <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
                 </svg>
-              </div>
-              <div className="player__btn-repeat _btn-icon">
-                <svg className="player__btn-repeat-svg" alt="repeat">
+              </StyledPlayerBtnNext>
+              <StyledPlayerBtnRepeat className="_btn-icon">
+                <StyledPlayerBtnRepeatSvg alt="repeat">
                   <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-                </svg>
-              </div>
-              <div className="player__btn-shuffle _btn-icon">
-                <svg className="player__btn-shuffle-svg" alt="shuffle">
+                </StyledPlayerBtnRepeatSvg>
+              </StyledPlayerBtnRepeat>
+              <StyledPlayerBtnShuffle className="_btn-icon">
+                <StyledPlayerBtnShuffleSvg alt="shuffle">
                   <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-                </svg>
-              </div>
-            </div>
+                </StyledPlayerBtnShuffleSvg>
+              </StyledPlayerBtnShuffle>
+            </StyledPlayerControls>
 
             <div className="player__track-play track-play">
               <div className="track-play__contain">
@@ -67,7 +191,7 @@ function Player() {
                 </div>
               </div>
             </div>
-          </div>
+          </StyledBarPlayer>
           <div className="bar__volume-block volume">
             <div className="volume__content">
               <div className="volume__image">
@@ -84,9 +208,9 @@ function Player() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </StyledBarPlayerBlock>
+      </StyledBarContent>
+    </StyledBar>
   )
 }
 
