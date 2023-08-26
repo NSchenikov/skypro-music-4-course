ae73efdf86380608b080068982fa52708f67e66f
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-26 23:54:28 +0300

    sidebar styled-component

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,48 +1,36 @@
-import './sidebar.css'
+import * as S from './sidebar.style'
 
 function Sidebar() {
   return (
-    <div className="main__sidebar sidebar">
-      <div className="sidebar__personal">
-        <p className="sidebar__personal-name">Sergey.Ivanov</p>
-        <div className="sidebar__icon">
-          <svg alt="logout" className="logout">
+    <S.MainSidebar>
+      <S.SidebarPersonal>
+        <S.SidebarPersonalName>Sergey.Ivanov</S.SidebarPersonalName>
+        <S.SidebarIcon>
+          <S.Logout alt="logout">
             <use xlinkHref="img/icon/sprite.svg#logout"></use>
-          </svg>
-        </div>
-      </div>
-      <div className="sidebar__block">
-        <div className="sidebar__list">
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist01.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div>
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist02.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div>
-          <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist03.png"
-                alt="day's playlist"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+          </S.Logout>
+        </S.SidebarIcon>
+      </S.SidebarPersonal>
+      <S.SidebarBlock>
+        <S.SidebarList>
+          <S.SidebarItem>
+            <S.SidebarLink href="#">
+              <S.SidebarImg src="img/playlist01.png" alt="day's playlist" />
+            </S.SidebarLink>
+          </S.SidebarItem>
+          <S.SidebarItem>
+            <S.SidebarLink href="#">
+              <S.SidebarImg src="img/playlist02.png" alt="day's playlist" />
+            </S.SidebarLink>
+          </S.SidebarItem>
+          <S.SidebarItem>
+            <S.SidebarLink href="#">
+              <S.SidebarImg src="img/playlist03.png" alt="day's playlist" />
+            </S.SidebarLink>
+          </S.SidebarItem>
+        </S.SidebarList>
+      </S.SidebarBlock>
+    </S.MainSidebar>
   )
 }
 
