cfe8e0a2443bdef7946c304d9af1d158cccbe8ad
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-09-30 17:40:56 +0300

    register and register validators working. Errors processed and renders in form

--- src/api.js
+++ src/api.js
@@ -11,18 +11,31 @@ export async function getTracks() {
   return data
 }
 
-export const registerUser = (email, password) => {
-  const promise = fetch('https://skypro-music-api.skyeng.tech/user/signup/', {
-    method: 'POST',
-    body: JSON.stringify({
-      email: email,
-      password: password,
-      username: email,
-    }),
-    headers: {
-      'content-type': 'application/json',
+export const registerUser = async (email, password) => {
+  const response = await fetch(
+    'https://skypro-music-api.skyeng.tech/user/signup/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        username: email,
+      }),
+      headers: {
+        'content-type': 'application/json',
+      },
     },
-  })
+  )
 
-  return promise
+  const data = await response.json()
+
+  if (!response.ok) {
+    console.log(data)
+    const error = data.email?.[0] ?? data.username?.[0] ?? data.password?.[0]
+    console.log(error)
+    throw new Error(error)
+  } else {
+    console.log(data)
+    return data
+  }
 }
