1c7858a39c9ecb63e603ea4667fd11f4914d80a9
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-10-01 15:27:10 +0300

    logout of sidebar working

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import * as S from './sidebar.style'
 
 export const categories = [1, 2, 3]
 
 function Sidebar() {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    localStorage.clear()
+    navigate('/login', { replace: true })
+  }
   return (
     <S.MainSidebar>
       <S.SidebarPersonal>
         <S.SidebarPersonalName>Sergey.Ivanov</S.SidebarPersonalName>
         <S.SidebarIcon>
-          <S.Logout alt="logout">
+          <S.Logout onClick={handleLogout} alt="logout">
             <use xlinkHref="img/icon/sprite.svg#logout"></use>
           </S.Logout>
         </S.SidebarIcon>
