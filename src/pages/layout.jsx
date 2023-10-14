ce6392f5cbafda373ec38d3fe096e8da2b501430
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-10-14 23:41:18 +0300

    layout like a parent component to main and favourites page. Favourites page working by Outlet. All the logic of Main page now in Layout

---
+++ src/pages/layout.jsx
@@ -0,0 +1,99 @@
+import MainNavMenu from '../components/mainNavMenu/mainNavMenu'
+import Search from '../components/search/search'
+import Filter from '../components/filter/filter'
+import Sidebar from '../components/sidebar/sidebar'
+import SidebarSkeleton from '../components/sidebarSkeleton/sidebarSkeleton'
+import PlayerSkeleton from '../components/playerSkeleton/playerSkeleton'
+import Player from '../components/player/player'
+import { getTracks } from '../api'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setChoosedTrack, setPlaylist } from '../store/tracksSlice'
+import * as S from './Main/index.style'
+import { Outlet } from 'react-router-dom'
+
+export const Layout = () => {
+  const [tracks, setTracks] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [fetchTracksError, setFetchTracksError] = useState(null)
+  let [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    getTracks()
+      .then((tracks) => {
+        setTracks(tracks)
+        // console.log(tracks)
+      })
+      .then(() => setLoading(false))
+      .catch((error) => {
+        setFetchTracksError(error.message)
+        setLoading(false)
+      })
+  }, [])
+  const currentTrk = useSelector((state) => state.trk)
+  const [trackIndex, setTrackIndex] = useState(null)
+  const [currentTrack, setCurrentTrack] = useState(currentTrk)
+  const dispatch = useDispatch()
+  const addSong = () => dispatch(setChoosedTrack(currentTrack))
+  const addPlayList = () => dispatch(setPlaylist(tracks))
+  addSong()
+  useEffect(() => {
+    addPlayList()
+  }, [currentTrack])
+  return (
+    <S.App>
+      <S.GlobalStyle />
+      <S.Wrapper>
+        <S.Container>
+          <S.Main>
+            <MainNavMenu />
+            <S.MainCenterblock>
+              <Search />
+              <S.CenterblockH2>Треки</S.CenterblockH2>
+              <Filter />
+              <S.CenterblockContent>
+                <S.ContentTitle>
+                  <S.Col01>Трек</S.Col01>
+                  <S.Col02>ИСПОЛНИТЕЛЬ</S.Col02>
+                  <S.Col03>АЛЬБОМ</S.Col03>
+                  <S.Col04>
+                    <S.PlaylistTitleSvg alt="time">
+                      <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+                    </S.PlaylistTitleSvg>
+                  </S.Col04>
+                </S.ContentTitle>
+                <Outlet
+                  context={[
+                    tracks,
+                    setCurrentTrack,
+                    currentTrack,
+                    isPlaying,
+                    setIsPlaying,
+                    setTrackIndex,
+                    fetchTracksError,
+                    loading,
+                  ]}
+                />
+              </S.CenterblockContent>
+            </S.MainCenterblock>
+            {loading && <SidebarSkeleton />}
+            {!loading && <Sidebar />}
+          </S.Main>
+          {loading && <PlayerSkeleton />}
+          {currentTrack && (
+            <Player
+              currentTrack={currentTrack}
+              isPlaying={isPlaying}
+              setIsPlaying={setIsPlaying}
+              tracks={tracks}
+              setCurrentTrack={setCurrentTrack}
+              trackIndex={trackIndex}
+              setTrackIndex={setTrackIndex}
+            />
+          )}
+          <S.Footer />
+        </S.Container>
+      </S.Wrapper>
+    </S.App>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,7 @@ import { Main } from './pages/Main/MainPage'
 import { Register } from './pages/register/register'
 import { NotFound } from './pages/not-found/NotFound'
 import { ProtectedRoute } from './components/protected-route/protectedRoute'
+import { Layout } from './pages/layout'
 // import user from './pages/login/login'
 
 export const AppRoutes = ({ user }) => {
@@ -15,28 +16,36 @@ export const AppRoutes = ({ user }) => {
       <Route path="/register" element={<Register />} />
 
       <Route
+        path="/"
+        element={
+          <ProtectedRoute user={user} isAllowed={Boolean(user)}>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route
+          index
+          element={
+            <ProtectedRoute user={user} isAllowed={Boolean(user)}>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="favourites"
+          element={
+            <ProtectedRoute user={user} isAllowed={Boolean(user)}>
+              <Favourites />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+
+      <Route
         path="/category/:id"
         element={
           <ProtectedRoute user={user} isAllowed={Boolean(user)}>
             <Category />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute user={user} isAllowed={Boolean(user)}>
-            <Main />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/favourites"
-        element={
-          <ProtectedRoute user={user} isAllowed={Boolean(user)}>
-            <Favourites />
           </ProtectedRoute>
         }
       />
